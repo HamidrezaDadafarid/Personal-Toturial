bab758c6f386ddea1326c6cd1775fa8b703cac6b
Hamidreza Dadafarid <hamidreza.dadafarid@aut.ac.ir>
2024-11-03 11:48:33 +0330

    aadded comments to 4.go

--- net-http/4 (serve a sibgle file).go
+++ net-http/4 (serve a sibgle file).go
@@ -5,15 +5,22 @@ import (
 	"net/http"
 )
 
+// main is the entry point for the application
 func main() {
+	// Define the file path for the file to be served
 	filePath := "./assets/1.png"
 
+	// Set up an HTTP handler function for the "/my-file" route
+	// When a request is made to "/my-file", this function will respond by serving the specified file
 	http.HandleFunc("/my-file", func(w http.ResponseWriter, r *http.Request) {
+		// Serve the file located at filePath to the client
 		http.ServeFile(w, r, filePath)
 	})
 
+	// Start the HTTP server on port 8080 and listen for incoming requests
+	// If an error occurs while starting the server, log the error and exit
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err) // Log the error if server fails to start
 	}
 }

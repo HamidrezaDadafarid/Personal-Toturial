bd3499b62fbe3062ca67ec6287bd8047310536a1
Hamidreza Dadafarid <hamidreza.dadafarid@aut.ac.ir>
2025-02-06 22:10:30 +0330

    chore: uncommented some lines

--- Golang Basics/Session 2/slices.go
+++ Golang Basics/Session 2/slices.go
@@ -15,15 +15,15 @@ func main() {
 	fmt.Println(len(v2), cap(v2)) // 10, 10
 
 	v2[9] = 99
-	// v2[10] = 100        // panic. we should append it
-	// fmt.Println(v2[10]) // panic again!
+	v2[10] = 100        // panic. we should append it
+	fmt.Println(v2[10]) // panic again!
 
 	v2 = append(v2, 100)
 	fmt.Println(v2[10]) // no more panic!
 
 	v3 := make([]int, 10, 20) // a slice with length of 10 and capacity of 20
 	fmt.Println(v3[9])
-	// fmt.Println(v3[10]) // slices range is from 0 to (length - 1)
+	fmt.Println(v3[10]) // slices range is from 0 to (length - 1). panic!
 
 	// when we append one or more elemtents to slice, if there is capacity for them it will be added. if not, a copy of that array with the length 2 times the current length will be made.
 	for i := range 10 {
